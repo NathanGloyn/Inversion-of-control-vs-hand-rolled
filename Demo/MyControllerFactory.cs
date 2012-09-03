6444c814b24abd483be561c53d00b6bbf83d1a01
NathanGloyn <nathans.dropbox@gmail.com>
2012-09-03 22:54:54 +0100

    Added additional controllers and code to enable to run

--- Demo/MyControllerFactory.cs
+++ Demo/MyControllerFactory.cs
@@ -20,15 +20,32 @@ namespace Demo
 
             var dataAccess = new DataAccess(connectionString);
 
-            var orderRepository = new OrderRepository(dataAccess, Mapper.Engine);
-            var customerRepository = new CustomerRepository(dataAccess, Mapper.Engine);
             var employeeRepository = new EmployeeRepository(dataAccess, Mapper.Engine);
+            var customerRepository = new CustomerRepository(dataAccess, Mapper.Engine);
             var shipperRepository = new ShipperRepository(dataAccess, Mapper.Engine);
 
-            var orderService = new OrderService(orderRepository, customerRepository, employeeRepository, shipperRepository);
 
             if (controllerType == typeof(OrderController))
+            {
+                var orderRepository = new OrderRepository(dataAccess, Mapper.Engine);
+                var orderService = new OrderService(orderRepository, customerRepository, employeeRepository, shipperRepository);
                 return new OrderController(orderRepository, orderService);
+            }
+
+            if (controllerType == typeof(CustomerController))
+            {
+                return new CustomerController(customerRepository);
+            }
+
+            if (controllerType == typeof(EmployeeController))
+            {
+                return new EmployeeController(employeeRepository);
+            }
+
+            if (controllerType == typeof(ShipperController))
+            {
+                return new ShipperController(shipperRepository);
+            }
 
             return base.GetControllerInstance(requestContext, controllerType);
         }
