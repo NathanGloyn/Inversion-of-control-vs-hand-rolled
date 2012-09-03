5bd86d4e63ba950dd4d808397664515381cf7654
NathanGloyn <nathans.dropbox@gmail.com>
2012-09-03 22:41:32 +0100

    Added OrderService and all dependencies required to enable application to run

--- Demo/Controllers/OrderController.cs
+++ Demo/Controllers/OrderController.cs
@@ -6,22 +6,55 @@ using System.Web;
 using System.Web.Mvc;
 using Demo.Interfaces;
 using Demo.Models;
+using Demo.ViewModel;
 
 namespace Demo.Controllers
 {
     public class OrderController : Controller
     {
         private IOrderRepository orderRepository;
+        private IOrderService orderService;
 
-        public OrderController(IOrderRepository repository)
+        public OrderController(IOrderRepository repository, IOrderService service)
         {
             orderRepository = repository;
+            orderService = service;
         }
 
         public ActionResult Index()
         {
             var orders = orderRepository.GetAll();
             return View(orders);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var order = orderRepository.GetById(Id);
+
+            return View(order);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var order = orderService.CreateViewModelForEdit(Id);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Order_EditViewModel toUpdate)
+        {
+            var validationErrors = orderService.Update(toUpdate);
+
+            if (validationErrors.Count == 0)
+                return RedirectToAction("Details", new { Id = toUpdate.OrderId });
+
+            foreach (var item in validationErrors)
+            {
+                ModelState.AddModelError(item.Key, item.Value);
+            }
+
+            return View(toUpdate);
         }
     }
 }
